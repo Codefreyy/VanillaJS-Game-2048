3f7bfa3975147115bbb609a2558f979f45498b7b
godfrey <joyyujiepeng@gmail.com>
2023-11-25 16:19:33 +0000

    feat: create Tile class

--- Grid.js
+++ Grid.js
@@ -3,27 +3,54 @@ const CELL_SIZE = 20
 const CELL_GAP = 2
 
 export default class Grid {
+  // private variable
+  #cells
+  get #emptyCells() {
+    return this.#cells.filter((cell) => cell.tile !== null)
+  }
   constructor(gridElement) {
     gridElement.style.setProperty("--grid-size", GRID_SIZE)
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
 
-    this.cells = createCellElements(gridElement).map((cellElement, index) => {
+    this.#cells = createCellElements(gridElement).map((cellElement, index) => {
       return new Cell(
         cellElement,
         index % GRID_SIZE,
         Math.floor(index / GRID_SIZE)
       )
     })
-    console.log(this.cells)
+
+    console.log(this.#cells)
+  }
+
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+    return this.#emptyCells[randomIndex]
   }
 }
 
 class Cell {
+  #cellElement
+  #x
+  #y
+  #tile
+  get tile() {
+    return this.#tile
+  }
+
+  set tile(value) {
+    this.#tile = value
+    if (value == null) return
+    // when clicking cell, assign cell position to title position
+    this.#tile.x = this.#x
+    this.#tile.y = this.#y
+  }
+
   constructor(cellElement, x, y) {
-    this.cellElement = cellElement
-    this.x = x
-    this.y = y
+    this.#cellElement = cellElement
+    this.#x = x
+    this.#y = y
   }
 }
 
